4da6f5cf9b9e925b40a990d8367fa156f710745a
memochou1993 <memochou1993@gmail.com>
2019-05-03 10:26:50 +0800

    Modify router

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/views/Home.vue';
 
 Vue.use(Router);
 
@@ -9,7 +8,9 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home,
+      redirect: {
+        name: 'user.keys',
+      },
     },
     {
       path: '/login',
